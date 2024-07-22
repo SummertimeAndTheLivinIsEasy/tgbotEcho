f66853632962ab1b680b27b00acaed6ef396e67b
SummertimeAndTheLivinIsEasy <165676989+SummertimeAndTheLivinIsEasy@users.noreply.github.com>
2024-07-22 23:16:29 +0300

    add main, config, env.example

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+
+
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
